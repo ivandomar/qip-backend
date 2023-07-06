dc2a7f0513507b7d1ffecfa5401899752fe4076d
Ivan Cézanne <ivancezanne.hrzon@gmail.com>
2023-07-05 21:44:58 -0300

    chore: adds docs homepage

--- app.py
+++ app.py
@@ -6,3 +6,5 @@ from urllib.parse import unquote
 info = Info(title="qip api", version="0.0.1")
 app = OpenAPI(__name__, info=info)
 CORS(app)
+
+home_tag = Tag(name="Documentation", description="Documentation pattern selection")
