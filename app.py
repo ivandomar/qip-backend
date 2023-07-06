7964cddd03ee515ebb00de489cb212c9eca092fe
Ivan Cézanne <ivancezanne.hrzon@gmail.com>
2023-07-05 22:04:01 -0300

    feature(app): adds home route redirecting to swagger api doc

--- app.py
+++ app.py
@@ -7,4 +7,8 @@ info = Info(title="qip api", version="0.0.1")
 app = OpenAPI(__name__, info=info)
 CORS(app)
 
-home_tag = Tag(name="Documentation", description="Documentation pattern selection")
+home_tag = Tag(name="Documentation", description="Documentation utilitary")
+
+@app.get('/', tags=[home_tag], summary="Shows API doc")
+def home():
+    return redirect('/openapi/swagger')
