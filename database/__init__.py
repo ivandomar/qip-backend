6d80474841e367e1994a72b3d84883431fccb53b
Ivan Cézanne <ivancezanne.hrzon@gmail.com>
2023-07-09 11:25:57 -0300

    feature(database): creates seeders

--- database/__init__.py
+++ database/__init__.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists, create_database
 import os
 
+from .seeders.element_type import element_type_seeder
 from models import Base, Element, ElementType
 
 db_path = "database/data"
@@ -14,9 +15,13 @@ db_url = f'sqlite:///{db_path}/qip.sqlite3'
 
 engine = create_engine(db_url, echo=True)
 
-session = sessionmaker(engine)
+Session = sessionmaker(engine)
 
 if not database_exists(engine.url):
     create_database(engine.url) 
 
 Base.metadata.create_all(engine)
+
+seeding_session = Session()
+seeding_session.add_all(element_type_seeder)
+seeding_session.commit()
---
+++ database/seeders/element_type.py
@@ -0,0 +1,6 @@
+from models.element_type import ElementType
+
+folder_type = ElementType('Folder')
+note_type = ElementType('Note')
+
+element_type_seeder = [folder_type, note_type]
