cfbbbe991dbb0e47627c68f224816cc540003c41
Ivan Cézanne <ivancezanne.hrzon@gmail.com>
2023-07-10 22:31:25 -0300

    feature(routes/element): declares remaining CRUD routes

--- routes/element.py
+++ routes/element.py
@@ -2,21 +2,53 @@ from flask_openapi3 import APIBlueprint, Tag
 
 from constants.http_statuses import OK, SEMANTIC_ERROR, SYNTAX_ERROR
 from controllers.element import add_element
-from schemas.responses.element import ElementResponseSchema
+from schemas.responses.element import ElementResponseSchema, ElementListResponseSchema
 from schemas.responses.general import ErrorResponseSchema
 
 element_blueprint = APIBlueprint("element", __name__, url_prefix="/elements")
 
 element_tag = Tag(name="Elements", description="CRUD of qip elements (notes and folders)")
 
-element_blueprint.post(
-    "/",
+element_blueprint.get(
+    '/<id>',
     tags=[element_tag],
-    summary="Creates new element",
+    summary='Gets element by its unique id',
     responses={
         str(OK): ElementResponseSchema,
         str(SEMANTIC_ERROR): ErrorResponseSchema,
         str(SYNTAX_ERROR): ErrorResponseSchema
         }
-    )(add_element)
+)
 
+element_blueprint.get(
+    '/<id>/elements',
+    tags=[element_tag],
+    summary='Gets elements of the specified parent element',
+    responses={
+        str(OK): ElementListResponseSchema,
+        str(SEMANTIC_ERROR): ErrorResponseSchema,
+        str(SYNTAX_ERROR): ErrorResponseSchema
+        }
+)
+
+element_blueprint.delete(
+    '/<id>',
+    tags=[element_tag],
+    summary='Removes the specified element from the database',
+    responses={
+        str(OK): None,
+        str(SEMANTIC_ERROR): ErrorResponseSchema,
+        str(SYNTAX_ERROR): ErrorResponseSchema
+        }
+)
+
+element_blueprint.post(
+    '/',
+    tags=[element_tag],
+    summary='Creates new element',
+    responses={
+        str(OK): ElementResponseSchema,
+        str(SEMANTIC_ERROR): ErrorResponseSchema,
+        str(SYNTAX_ERROR): ErrorResponseSchema
+        }
+)(add_element)
