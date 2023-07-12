7a5df5df556779aa0326abff3034f9a3c1c6bcc2
Ivan Cézanne <ivancezanne.hrzon@gmail.com>
2023-07-11 22:15:21 -0300

    feature(controllers/element): implements parent existence checking in creation and query

--- constants/error_messages.py
+++ constants/error_messages.py
@@ -1,2 +1,3 @@
 DUPLICATED_ELEMENT = 'Such title already exists in this folder'
+FOLDER_NOT_FOUND = 'Such parent element was not found in the database'
 GENERAL_ERROR = 'Could not process this request'
--- controllers/element.py
+++ controllers/element.py
@@ -1,6 +1,6 @@
 from sqlalchemy.exc import IntegrityError
 
-from constants.error_messages import DUPLICATED_ELEMENT, GENERAL_ERROR
+from constants.error_messages import DUPLICATED_ELEMENT, FOLDER_NOT_FOUND, GENERAL_ERROR
 from constants.http_statuses import OK, CREATED, SEMANTIC_ERROR, SYNTAX_ERROR
 from database import Session
 from formatters.element import format_element_response, format_element_list_response
@@ -14,6 +14,11 @@ def add(form: NewElementRequestSchema):
     
     try:
         session = Session()
+
+        parent = session.query(Element).filter(Element.id == form.parent_id).one_or_none()
+
+        if parent is None:
+            raise IntegrityError(FOLDER_NOT_FOUND)
         
         matching_element = session.query(Element).filter(
             Element.parent_id == new_element.parent_id,
@@ -40,12 +45,21 @@ def get_by_folder(form: ListElementRequestSchema):
     
     try:
         session = Session()
+
+        parent = session.query(Element).filter(Element.id == parent_id).one_or_none()
+
+        if parent is None:
+            raise IntegrityError(FOLDER_NOT_FOUND)
+
         elements = session.query(Element).filter(
             Element.parent_id == parent_id,
             Element.deleted_at == None
         ).all()
         
         return format_element_list_response(elements), OK
+
+    except IntegrityError as e:
+        return {"mesage": str(e)}, SEMANTIC_ERROR
 
     except Exception as e:        
         return {"mesage": GENERAL_ERROR}, SYNTAX_ERROR
